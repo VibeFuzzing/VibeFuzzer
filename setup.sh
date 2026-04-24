b9f5adaa28d204cf8e2bfe5d3db0d0b51d0195e5
Haeleon <30844056+Haeleon@users.noreply.github.com>
2026-04-24 16:06:30 -0500

    Combine apt-get calls

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 currentdir=`pwd`
 
 sudo apt-get update
-sudo apt-get install -y git curl python3-venv meson libcjson-dev libcurl4-openssl-dev tmux
-
-sudo apt-get install -y build-essential python3-dev automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools cargo libgtk-3-dev
-# sudo apt-get install -y lld-14 llvm-14 llvm-14-dev clang-14 || sudo apt-get install -y lld llvm llvm-dev clang
-sudo apt-get install -y lld llvm llvm-dev clang
-sudo apt-get install -y gcc-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-plugin-dev libstdc++-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-dev
-sudo apt-get install -y ninja-build # for QEMU mode
+sudo apt-get install -y git curl python3-venv meson libcjson-dev libcurl4-openssl-dev tmux build-essential python3-dev automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools cargo libgtk-3-dev lld llvm llvm-dev clang gcc-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-plugin-dev libstdc++-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-dev ninja-build
 
 curl -fsSL https://ollama.com/install.sh | sh
 
